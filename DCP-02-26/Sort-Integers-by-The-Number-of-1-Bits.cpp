806525ff1293dfb2361686aca5f3a3185a34561e
Abhishek <abhimallav1439@gmail.com>
2026-02-25 11:47:19 +0530

    [1356] [Time Beats: 57.35%] [Memory Beats: 5.10%] - LeetPush

--- DCP-02-26/Sort-Integers-by-The-Number-of-1-Bits.cpp
+++ DCP-02-26/Sort-Integers-by-The-Number-of-1-Bits.cpp
@@ -1,16 +1,14 @@
 1class Solution {
 2public:
 3    vector<int> sortByBits(vector<int>& arr) {
-4        vector<vector<int>>vec;
-5        vector<int>ans;
-6        for(int i=0;i<arr.size();i++){
-7            int ones=__builtin_popcount(arr[i]);
-8            vec.push_back({ones,arr[i]});
-9        }
-10        sort(vec.begin(),vec.end());
-11        for(auto i:vec){
-12            ans.push_back(i[1]);
-13        }
-14        return ans;
-15    }
-16};
+4        function<bool(int,int)> comp = [](int a, int b) {
+5            int countA = __builtin_popcount(a);
+6            int countB = __builtin_popcount(b);
+7            if (countA == countB)
+8                return a < b;
+9            return countA < countB;
+10        };
+11        sort(arr.begin(), arr.end(),comp);
+12        return arr;
+13    }
+14};
