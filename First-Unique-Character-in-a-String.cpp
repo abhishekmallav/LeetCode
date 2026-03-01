ad26e7e0acbca9018bb74118eeff21302247a29a
Abhishek <abhimallav1439@gmail.com>
2026-03-01 11:18:27 +0530

    [387] [Time Beats: 37.52%] [Memory Beats: 20.66%] - LeetPush

---
+++ First-Unique-Character-in-a-String.cpp
@@ -0,0 +1,15 @@
+1class Solution {
+2public:
+3    int firstUniqChar(string s) {
+4        unordered_map<char, int> freq;
+5        for (char c : s) {
+6            freq[c]++;
+7        }
+8        for (int i = 0; i < s.length(); i++) {
+9            if (freq[s[i]] == 1) {
+10                return i;
+11            }
+12        }
+13        return -1;
+14    }
+15};
