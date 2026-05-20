ec40bbf677b13528029a85e317530302eaf24bcf
Abhishek <abhimallav1439@gmail.com>
2026-05-20 14:19:58 +0530

    [367] [Time Beats: 100.00%] [Memory Beats: 50.64%] - LeetPush

---
+++ Valid-Perfect-Square.cpp
@@ -0,0 +1,17 @@
+        long L = 1, R = num;
+
+        while (L <= R) {
+            long mid = L + (R - L) / 2;
+            long square = mid * mid;
+
+            if (square == num) {
+                return true;
+            } else if (square < num) {
+            return true;
+
+            return false;
+        if (num == 1)
+public:
+    bool isPerfectSquare(int num) {
+        if (num < 1)
+class Solution {
