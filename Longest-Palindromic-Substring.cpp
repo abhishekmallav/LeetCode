50140785b73bb3dbd34e74933a3b0257e7c7b824
Abhishek <abhimallav1439@gmail.com>
2026-05-27 14:30:58 +0530

    [5] [Time Beats: 0%] [Memory Beats: 96.49%] - LeetPush

---
+++ Longest-Palindromic-Substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool solve(string& s, int i, int j) {
+        if (i >= j)
+            return true;
+
+        if (s[i] == s[j])
+            return solve(s, i+1, j-1);
+        return false;
+    }
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int maxi = INT_MIN;
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solve(s, i, j)) {
+                    if (j - i + 1 > maxi) {
+                        maxi = j - i + 1;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substr(start, maxi);
+    }
+};
