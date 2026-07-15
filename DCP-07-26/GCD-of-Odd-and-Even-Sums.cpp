8673dd3ef9d77a1334ce07bc309e8cc34741fdc0
Abhishek <abhimallav1439@gmail.com>
2026-07-15 11:32:21 +0530

    [3658] [Time Beats: 0%] [Memory Beats: 100.00%] - LeetPush

---
+++ DCP-07-26/GCD-of-Odd-and-Even-Sums.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        // The GCD of the first n odd sums (n^2)
+        // and the first n even sums (n*(n+1)) is always n.
+        return n;
+    }
+};
